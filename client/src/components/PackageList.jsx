09adc97b6407dca6330c082b03289609e10909ae
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:04:11 +0600

    Update PackageList.jsx

--- client/src/components/PackageList.jsx
+++ client/src/components/PackageList.jsx
@@ -5,10 +5,12 @@ function PackageList() {
     <div>
       <h2>Diamond Packages</h2>
 
-      <DiamondCard />
-      <DiamondCard />
-      <DiamondCard />
-      <DiamondCard />
+      <DiamondCard diamond="25" price="25" />
+      <DiamondCard diamond="50" price="50" />
+      <DiamondCard diamond="115" price="110" />
+      <DiamondCard diamond="240" price="220" />
+      <DiamondCard diamond="420" price="500" />
+      <DiamondCard diamond="840" price="1000" />
     </div>
   );
 }
