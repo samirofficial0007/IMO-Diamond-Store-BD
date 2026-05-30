0816e902d941b9046037204ba756538584b79167
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 19:44:43 +0600

    Update DiamondCard.jsx

--- client/src/components/DiamondCard.jsx
+++ client/src/components/DiamondCard.jsx
@@ -1,7 +1,18 @@
 function DiamondCard({ diamond, price }) {
 return (
 <div className="package-card">
-<h3>{diamond} Diamond</h3>
+<div
+style={{
+fontSize: "34px",
+marginBottom: "10px",
+color: "#CBE6F2",
+textShadow: "0 0 12px rgba(166,210,230,0.9)"
+}}
+>
+◈
+</div>
+
+  <h3>{diamond} Diamond</h3>
 
   <p>{price} BDT</p>
 
