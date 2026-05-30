09998c114b3d55036f4e908fde86c79ddce6c97f
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:47:35 +0600

    Update adminRoutes.js

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,1 +1,12 @@
+const adminController = require("../controllers/adminController");
 
+const adminRoutes = {
+  adminLogin: adminController.adminLogin,
+  getDashboardStats: adminController.getDashboardStats,
+  getSiteSettings: adminController.getSiteSettings,
+  updateSiteSettings: adminController.updateSiteSettings,
+  enableMaintenance: adminController.enableMaintenance,
+  disableMaintenance: adminController.disableMaintenance
+};
+
+module.exports = adminRoutes;
