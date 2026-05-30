7544a79c056c4286a3f8ef31040c071460f60cc7
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:17:08 +0600

    Update PaymentSetting.js

--- server/models/PaymentSetting.js
+++ server/models/PaymentSetting.js
@@ -1,1 +1,22 @@
+class PaymentSetting {
+  constructor() {
+    this.bkashNumber = "";
 
+    this.nagadNumber = "";
+
+    this.binancePayId = "895171022";
+
+    this.bkashInstructions =
+      "Send payment using bKash and enter your Transaction ID.";
+
+    this.nagadInstructions =
+      "Send payment using Nagad and enter your Transaction ID.";
+
+    this.binanceInstructions =
+      "Send payment using Binance Pay and enter your Binance Order ID.";
+
+    this.updatedAt = new Date();
+  }
+}
+
+module.exports = PaymentSetting;
