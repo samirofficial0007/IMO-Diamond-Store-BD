ff9e5e25f98fe7f1de04812155647cc3bcdd33dc
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 19:15:22 +0600

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,36 @@
 function Footer() {
-  return (
-    <footer>
-      <hr />
+return (
+<footer
+style={{
+background: "#181818",
+borderTop: "2px solid #ff7a00",
+marginTop: "20px",
+padding: "20px",
+textAlign: "center"
+}}
+>
+<h3 style={{ color: "#ff7a00" }}>
+💎 IMO Diamond Store BD
+</h3>
 
-      <p>IMO Diamond Store BD</p>
+  <p style={{ marginTop: "10px" }}>
+    Trusted & Fast Diamond Seller
+  </p>
 
-      <p>Telegram Group Available</p>
+  <p style={{ marginTop: "10px" }}>
+    Telegram Group Available
+  </p>
 
-      <p>Discord: panelvai07</p>
+  <p>
+    Discord: panelvai07
+  </p>
 
-      <p>© All Rights Reserved</p>
-    </footer>
-  );
+  <p style={{ marginTop: "15px", fontSize: "14px" }}>
+    © All Rights Reserved
+  </p>
+</footer>
+
+);
 }
 
 export default Footer;
