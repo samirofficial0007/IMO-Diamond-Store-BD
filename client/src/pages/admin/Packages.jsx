80ccfdf5a0d0b2738b61bf4e6033767c95f7fdf5
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 06:41:38 +0600

    Update Packages.jsx

--- client/src/pages/admin/Packages.jsx
+++ client/src/pages/admin/Packages.jsx
@@ -1,1 +1,51 @@
+function Packages() {
+  return (
+    <div>
+      <h1>Manage Diamond Packages</h1>
 
+      <hr />
+
+      <h2>Add New Package</h2>
+
+      <input
+        type="text"
+        placeholder="Diamond Amount"
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="text"
+        placeholder="Price (BDT)"
+      />
+
+      <br />
+      <br />
+
+      <button>Add Package</button>
+
+      <hr />
+
+      <h2>Current Packages</h2>
+
+      <div>
+        <p>420 Diamond - 500 BDT</p>
+
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+
+      <hr />
+
+      <div>
+        <p>840 Diamond - 1000 BDT</p>
+
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Packages;
