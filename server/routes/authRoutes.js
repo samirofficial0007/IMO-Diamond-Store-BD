c509d2334d61f815cf887bfa3aa51499f69981e4
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:21:25 +0600

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,1 +1,10 @@
+const authController = require("../controllers/authController");
 
+const authRoutes = {
+  register: authController.register,
+  login: authController.login,
+  googleLogin: authController.googleLogin,
+  logout: authController.logout
+};
+
+module.exports = authRoutes;
