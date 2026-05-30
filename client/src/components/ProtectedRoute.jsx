184f1deff917cd4712bb5bd0dcd3e5c526ec9253
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 07:15:41 +0600

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,1 +1,16 @@
+function ProtectedRoute({ children }) {
+  const isLoggedIn = false;
 
+  if (!isLoggedIn) {
+    return (
+      <div>
+        <h2>Login Required</h2>
+        <p>Please login to continue.</p>
+      </div>
+    );
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
