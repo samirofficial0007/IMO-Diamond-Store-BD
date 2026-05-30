4dc4201304d28c53a9876a065b1137bae302ce9b
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 06:29:53 +0600

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,1 +1,37 @@
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
 
+      <input
+        type="text"
+        placeholder="Email or Username"
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+      />
+
+      <br />
+      <br />
+
+      <button>Login</button>
+
+      <br />
+      <br />
+
+      <button>Continue with Google</button>
+
+      <br />
+      <br />
+
+      <a href="#">Forgot Password?</a>
+    </div>
+  );
+}
+
+export default Login;
