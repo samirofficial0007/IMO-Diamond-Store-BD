9b08e8fae186b21830904a15e92b121f88e843e1
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:15:06 +0600

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,22 @@
+class User {
+  constructor() {
+    this.id = "";
+    this.username = "";
+    this.email = "";
+    this.password = "";
 
+    this.googleAccount = false;
+
+    this.role = "user";
+
+    this.language = "en";
+
+    this.notifications = [];
+
+    this.orders = [];
+
+    this.createdAt = new Date();
+  }
+}
+
+module.exports = User;
