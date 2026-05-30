f818f4bee724916311ab9c126f904a773fbb5b0c
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:45:43 +0600

    Update notificationRoutes.js

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -1,1 +1,10 @@
+const notificationController = require("../controllers/notificationController");
 
+const notificationRoutes = {
+  sendNotification: notificationController.sendNotification,
+  getUserNotifications: notificationController.getUserNotifications,
+  markAsRead: notificationController.markAsRead,
+  deleteNotification: notificationController.deleteNotification
+};
+
+module.exports = notificationRoutes;
