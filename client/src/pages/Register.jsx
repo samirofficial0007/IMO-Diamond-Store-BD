30d8c228331ce1208d421828902084a92854a858
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 06:31:06 +0600

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,1 +1,48 @@
+function Register() {
+  return (
+    <div>
+      <h1>Create Account</h1>
 
+      <input
+        type="text"
+        placeholder="Username"
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="email"
+        placeholder="Email"
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Confirm Password"
+      />
+
+      <br />
+      <br />
+
+      <button>Create Account</button>
+
+      <br />
+      <br />
+
+      <button>Continue with Google</button>
+    </div>
+  );
+}
+
+export default Register;
