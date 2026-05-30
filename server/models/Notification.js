b3f9a5e7227a10f6ac1105ee2fd9c21d7b657f6e
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:17:57 +0600

    Update Notification.js

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -1,1 +1,25 @@
+class Notification {
+  constructor() {
+    this.id = "";
 
+    this.userId = "";
+
+    this.title = "";
+
+    this.message = "";
+
+    this.type = "general";
+    // general
+    // order_completed
+    // processing
+    // payment_not_found
+    // new_offer
+    // admin_notice
+
+    this.isRead = false;
+
+    this.createdAt = new Date();
+  }
+}
+
+module.exports = Notification;
