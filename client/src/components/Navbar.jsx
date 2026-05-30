8f39e780a3790e840f48ed6c86359d50ed02f97e
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 06:27:47 +0600

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,1 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>IMO Diamond Store BD</h2>
 
+      <div>
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
