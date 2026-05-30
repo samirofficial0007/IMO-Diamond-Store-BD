ee9ca4c67e9e5fea9368cd8dfa2e1d66ff51fb65
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:44:52 +0600

    Update notificationController.js

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -1,1 +1,36 @@
+const Notification = require("../models/Notification");
 
+const sendNotification = (req, res) => {
+  res.json({
+    success: true,
+    message: "Send Notification endpoint ready"
+  });
+};
+
+const getUserNotifications = (req, res) => {
+  res.json({
+    success: true,
+    message: "Get User Notifications endpoint ready"
+  });
+};
+
+const markAsRead = (req, res) => {
+  res.json({
+    success: true,
+    message: "Mark As Read endpoint ready"
+  });
+};
+
+const deleteNotification = (req, res) => {
+  res.json({
+    success: true,
+    message: "Delete Notification endpoint ready"
+  });
+};
+
+module.exports = {
+  sendNotification,
+  getUserNotifications,
+  markAsRead,
+  deleteNotification
+};
