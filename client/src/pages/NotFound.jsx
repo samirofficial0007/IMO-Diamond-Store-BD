5d0e83ffd688148ccc05eef4dcfe1f7f76bd3537
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:10:47 +0600

    Update NotFound.jsx

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,1 +1,17 @@
+function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
 
+      <h2>Page Not Found</h2>
+
+      <p>
+        The page you are looking for does not exist.
+      </p>
+
+      <button>Go Home</button>
+    </div>
+  );
+}
+
+export default NotFound;
