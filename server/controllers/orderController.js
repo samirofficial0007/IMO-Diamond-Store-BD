9c3ef66b34bbc5e8883db1852bbc4e77cd9378ad
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:22:32 +0600

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,1 +1,44 @@
+const Order = require("../models/Order");
 
+const createOrder = (req, res) => {
+  res.json({
+    success: true,
+    message: "Create Order endpoint ready"
+  });
+};
+
+const getUserOrders = (req, res) => {
+  res.json({
+    success: true,
+    message: "Get User Orders endpoint ready"
+  });
+};
+
+const updateOrderStatus = (req, res) => {
+  res.json({
+    success: true,
+    message: "Update Order Status endpoint ready"
+  });
+};
+
+const approveOrder = (req, res) => {
+  res.json({
+    success: true,
+    message: "Approve Order endpoint ready"
+  });
+};
+
+const paymentNotFound = (req, res) => {
+  res.json({
+    success: true,
+    message: "Payment Not Found endpoint ready"
+  });
+};
+
+module.exports = {
+  createOrder,
+  getUserOrders,
+  updateOrderStatus,
+  approveOrder,
+  paymentNotFound
+};
