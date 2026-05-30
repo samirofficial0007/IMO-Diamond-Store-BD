2df292c78ed0093b374a1b2e66c4e6548a78e6a7
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 06:39:09 +0600

    Update Login.jsx

--- client/src/pages/admin/Login.jsx
+++ client/src/pages/admin/Login.jsx
@@ -1,1 +1,39 @@
+function AdminLogin() {
+  return (
+    <div>
+      <h1>Admin Login</h1>
 
+      <p>Authorized Access Only</p>
+
+      <br />
+
+      <input
+        type="text"
+        placeholder="Admin Username"
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Admin Password"
+      />
+
+      <br />
+      <br />
+
+      <button>Login</button>
+
+      <br />
+      <br />
+
+      <p>
+        Hidden Route:
+        /imo-samir-admin-0007
+      </p>
+    </div>
+  );
+}
+
+export default AdminLogin;
