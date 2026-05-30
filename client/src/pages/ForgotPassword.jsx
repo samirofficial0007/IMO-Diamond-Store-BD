a66578017297f90f8484ac7e2f3136d8a75918ce
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 06:32:05 +0600

    Update ForgotPassword.jsx

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,1 +1,23 @@
+function ForgotPassword() {
+  return (
+    <div>
+      <h1>Forgot Password</h1>
 
+      <p>
+        Enter your email address to receive a password reset link.
+      </p>
+
+      <input
+        type="email"
+        placeholder="Enter your Email"
+      />
+
+      <br />
+      <br />
+
+      <button>Send Reset Link</button>
+    </div>
+  );
+}
+
+export default ForgotPassword;
