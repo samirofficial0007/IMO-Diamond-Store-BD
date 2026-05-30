c10c08757ffd38c21d77166321de223fc87c3921
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 06:26:44 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,7 @@
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div>
-      <h1>IMO Diamond Store BD</h1>
-      <p>Website Setup Successful</p>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
