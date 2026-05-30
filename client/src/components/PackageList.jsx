902738552a1d3312bd05f90c7be0f7e01764326b
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 07:20:52 +0600

    Update PackageList.jsx

--- client/src/components/PackageList.jsx
+++ client/src/components/PackageList.jsx
@@ -1,1 +1,16 @@
+import DiamondCard from "./DiamondCard";
 
+function PackageList() {
+  return (
+    <div>
+      <h2>Diamond Packages</h2>
+
+      <DiamondCard />
+      <DiamondCard />
+      <DiamondCard />
+      <DiamondCard />
+    </div>
+  );
+}
+
+export default PackageList;
