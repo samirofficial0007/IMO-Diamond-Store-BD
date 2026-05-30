9266a256191e68a0427c38a5349bf46341e2fba6
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:41:26 +0600

    Update packageRoutes.js

--- server/routes/packageRoutes.js
+++ server/routes/packageRoutes.js
@@ -1,1 +1,10 @@
+const packageController = require("../controllers/packageController");
 
+const packageRoutes = {
+  getPackages: packageController.getPackages,
+  addPackage: packageController.addPackage,
+  updatePackage: packageController.updatePackage,
+  deletePackage: packageController.deletePackage
+};
+
+module.exports = packageRoutes;
