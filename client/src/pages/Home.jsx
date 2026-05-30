46dfabbe1b45e833061ff455789b2fbe0b63e514
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:02:21 +0600

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,9 @@ import Navbar from "../components/Navbar";
 import LanguageSwitcher from "../components/LanguageSwitcher";
 import PackageList from "../components/PackageList";
 import WarningNotice from "../components/WarningNotice";
+import NotificationBox from "../components/NotificationBox";
+import PaymentMethod from "../components/PaymentMethod";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
@@ -14,9 +17,15 @@ function Home() {
 
       <p>Trusted IMO Diamond Seller</p>
 
+      <NotificationBox />
+
       <PackageList />
 
+      <PaymentMethod />
+
       <WarningNotice />
+
+      <Footer />
     </div>
   );
 }
