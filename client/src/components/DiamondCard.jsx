33374ce9f6fb72e87e8def6407e8c3217814548e
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 07:20:14 +0600

    Update DiamondCard.jsx

--- client/src/components/DiamondCard.jsx
+++ client/src/components/DiamondCard.jsx
@@ -1,1 +1,13 @@
+function DiamondCard() {
+  return (
+    <div>
+      <h3>420 Diamond</h3>
 
+      <p>Price: 500 BDT</p>
+
+      <button>Buy Now</button>
+    </div>
+  );
+}
+
+export default DiamondCard;
