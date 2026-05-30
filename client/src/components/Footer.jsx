d545e347dad931499beb44915b146b9ba403cd68
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 07:58:53 +0600

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,1 +1,15 @@
+function Footer() {
+  return (
+    <footer>
+      <hr />
 
+      <p>IMO Diamond Store BD</p>
+
+      <p>Telegram | Facebook | Discord</p>
+
+      <p>© All Rights Reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
