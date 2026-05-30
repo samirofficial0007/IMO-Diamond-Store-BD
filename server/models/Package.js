a97e501211eb81c6ba4fdc0a6074b48b46494d45
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:40:31 +0600

    Update Package.js

--- server/models/Package.js
+++ server/models/Package.js
@@ -1,1 +1,17 @@
+class Package {
+  constructor() {
+    this.id = "";
 
+    this.diamondAmount = 0;
+
+    this.price = 0;
+
+    this.isActive = true;
+
+    this.createdAt = new Date();
+
+    this.updatedAt = new Date();
+  }
+}
+
+module.exports = Package;
