f64c6426c0d1ba7e68002cf7a6e0fc249737b575
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:15:55 +0600

    Update Order.js

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,1 +1,35 @@
+class Order {
+  constructor() {
+    this.id = "";
 
+    this.userId = "";
+
+    this.imoId = "";
+
+    this.diamondAmount = 0;
+
+    this.price = 0;
+
+    this.paymentMethod = "";
+
+    this.transactionId = "";
+
+    this.binanceOrderId = "";
+
+    this.screenshot = "";
+
+    this.status = "pending";
+    // pending
+    // processing
+    // completed
+    // payment_not_found
+
+    this.adminNote = "";
+
+    this.createdAt = new Date();
+
+    this.updatedAt = new Date();
+  }
+}
+
+module.exports = Order;
