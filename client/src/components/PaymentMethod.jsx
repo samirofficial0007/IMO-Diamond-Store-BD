1054bbd58fa79def11f607b97fe3fc5ab3542bb5
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 19:08:19 +0600

    Update PaymentMethod.jsx

--- client/src/components/PaymentMethod.jsx
+++ client/src/components/PaymentMethod.jsx
@@ -1,15 +1,26 @@
 function PaymentMethod() {
-  return (
-    <div>
-      <h2>Payment Methods</h2>
+return (
+<div
+style={{
+background: "#1b1b1b",
+border: "1px solid #333",
+borderRadius: "12px",
+margin: "15px",
+padding: "15px"
+}}
+>
+<h2 style={{ color: "#ff7a00", marginBottom: "10px" }}>
+💳 Payment Methods
+</h2>
 
-      <ul>
-        <li>bKash</li>
-        <li>Nagad</li>
-        <li>Binance Pay</li>
-      </ul>
-    </div>
-  );
+  <ul style={{ paddingLeft: "20px" }}>
+    <li>bKash</li>
+    <li>Nagad</li>
+    <li>Binance Pay</li>
+  </ul>
+</div>
+
+);
 }
 
 export default PaymentMethod;
