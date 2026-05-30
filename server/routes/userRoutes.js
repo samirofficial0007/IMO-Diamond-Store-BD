6875545f4ac358ceaf518f340e09d37f142bd283
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:49:37 +0600

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,1 +1,10 @@
+const userController = require("../controllers/userController");
 
+const userRoutes = {
+  getProfile: userController.getProfile,
+  updateProfile: userController.updateProfile,
+  changeLanguage: userController.changeLanguage,
+  deleteAccount: userController.deleteAccount
+};
+
+module.exports = userRoutes;
