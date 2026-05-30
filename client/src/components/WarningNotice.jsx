1e13c5f63d058777a85f4fe4b9da6656c7547e64
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 07:21:34 +0600

    Update WarningNotice.jsx

--- client/src/components/WarningNotice.jsx
+++ client/src/components/WarningNotice.jsx
@@ -1,1 +1,17 @@
+function WarningNotice() {
+  return (
+    <div>
+      <h2>Important Notice</h2>
 
+      <ul>
+        <li>Check your IMO ID carefully.</li>
+        <li>Transaction ID must be correct.</li>
+        <li>Duplicate Transaction IDs are not allowed.</li>
+        <li>Orders are manually verified.</li>
+        <li>Fake payments will be rejected.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default WarningNotice;
