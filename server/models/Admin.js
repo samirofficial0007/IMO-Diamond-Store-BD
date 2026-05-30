1dff21f7883bc00b3fe372937d697c26963f4419
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:18:57 +0600

    Update Admin.js

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,1 +1,26 @@
+class Admin {
+  constructor() {
+    this.id = "";
 
+    this.username = "";
+
+    this.email = "";
+
+    this.role = "admin";
+
+    this.permissions = [
+      "manage_orders",
+      "manage_packages",
+      "manage_payments",
+      "manage_users",
+      "manage_notifications",
+      "manage_settings"
+    ];
+
+    this.lastLogin = null;
+
+    this.createdAt = new Date();
+  }
+}
+
+module.exports = Admin;
