d54f04d06f7584265f9c8ac8c47d20bf20c96254
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 06:25:20 +0600

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,1 +1,28 @@
+function Home() {
+  return (
+    <div>
+      <h1>IMO Diamond Store BD</h1>
 
+      <p>Trusted IMO Diamond Seller</p>
+
+      <h2>Popular Packages</h2>
+
+      <div>
+        <p>10 Diamond - 50 BDT</p>
+        <button>Buy Now</button>
+      </div>
+
+      <div>
+        <p>42 Diamond - 141 BDT</p>
+        <button>Buy Now</button>
+      </div>
+
+      <div>
+        <p>84 Diamond - 280 BDT</p>
+        <button>Buy Now</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
