1faa5a7099cc6edc83f942a3378937f39d78abdb
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:43:51 +0600

    Update paymentRoutes.js

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,1 +1,11 @@
+const paymentController = require("../controllers/paymentController");
 
+const paymentRoutes = {
+  getPaymentSettings: paymentController.getPaymentSettings,
+  updateBkashNumber: paymentController.updateBkashNumber,
+  updateNagadNumber: paymentController.updateNagadNumber,
+  updateBinancePayId: paymentController.updateBinancePayId,
+  validateTransactionId: paymentController.validateTransactionId
+};
+
+module.exports = paymentRoutes;
