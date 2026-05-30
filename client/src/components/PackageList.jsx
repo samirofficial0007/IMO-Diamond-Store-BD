d33d091616552039387afc6b0c2b0d4f35d25e4b
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:08:06 +0600

    Update PackageList.jsx

--- client/src/components/PackageList.jsx
+++ client/src/components/PackageList.jsx
@@ -1,16 +1,32 @@
 import DiamondCard from "./DiamondCard";
 
 function PackageList() {
+  const packages = [
+    { diamond: 25, price: 25 },
+    { diamond: 50, price: 50 },
+    { diamond: 115, price: 110 },
+    { diamond: 240, price: 220 },
+    { diamond: 355, price: 330 },
+    { diamond: 420, price: 500 },
+    { diamond: 610, price: 650 },
+    { diamond: 840, price: 1000 },
+    { diamond: 1240, price: 1400 },
+    { diamond: 2530, price: 2800 },
+    { diamond: 5060, price: 5500 },
+    { diamond: 10120, price: 11000 }
+  ];
+
   return (
     <div>
       <h2>Diamond Packages</h2>
 
-      <DiamondCard diamond="25" price="25" />
-      <DiamondCard diamond="50" price="50" />
-      <DiamondCard diamond="115" price="110" />
-      <DiamondCard diamond="240" price="220" />
-      <DiamondCard diamond="420" price="500" />
-      <DiamondCard diamond="840" price="1000" />
+      {packages.map((pkg, index) => (
+        <DiamondCard
+          key={index}
+          diamond={pkg.diamond}
+          price={pkg.price}
+        />
+      ))}
     </div>
   );
 }
