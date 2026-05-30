d795642ade2936cf4115f6165b6e05ed9d3c0f64
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 14:36:06 +0600

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173
-  }
+  plugins: [react()]
 });
