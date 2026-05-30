796c4824a8be86d92ff8a35d10e6cbcda4333d56
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:42:34 +0600

    Update paymentController.js

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,1 +1,44 @@
+const PaymentSetting = require("../models/PaymentSetting");
 
+const getPaymentSettings = (req, res) => {
+  res.json({
+    success: true,
+    message: "Get Payment Settings endpoint ready"
+  });
+};
+
+const updateBkashNumber = (req, res) => {
+  res.json({
+    success: true,
+    message: "Update bKash Number endpoint ready"
+  });
+};
+
+const updateNagadNumber = (req, res) => {
+  res.json({
+    success: true,
+    message: "Update Nagad Number endpoint ready"
+  });
+};
+
+const updateBinancePayId = (req, res) => {
+  res.json({
+    success: true,
+    message: "Update Binance Pay ID endpoint ready"
+  });
+};
+
+const validateTransactionId = (req, res) => {
+  res.json({
+    success: true,
+    message: "Validate Transaction ID endpoint ready"
+  });
+};
+
+module.exports = {
+  getPaymentSettings,
+  updateBkashNumber,
+  updateNagadNumber,
+  updateBinancePayId,
+  validateTransactionId
+};
