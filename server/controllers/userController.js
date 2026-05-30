dae94ea4adb4f2b1450089f0375d9a3d74998375
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:48:45 +0600

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,36 @@
+const User = require("../models/User");
 
+const getProfile = (req, res) => {
+  res.json({
+    success: true,
+    message: "Get Profile endpoint ready"
+  });
+};
+
+const updateProfile = (req, res) => {
+  res.json({
+    success: true,
+    message: "Update Profile endpoint ready"
+  });
+};
+
+const changeLanguage = (req, res) => {
+  res.json({
+    success: true,
+    message: "Change Language endpoint ready"
+  });
+};
+
+const deleteAccount = (req, res) => {
+  res.json({
+    success: true,
+    message: "Delete Account endpoint ready"
+  });
+};
+
+module.exports = {
+  getProfile,
+  updateProfile,
+  changeLanguage,
+  deleteAccount
+};
