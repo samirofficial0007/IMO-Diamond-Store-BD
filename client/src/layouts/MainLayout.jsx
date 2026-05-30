8ced1d1b0438ec137fe783858f266feb57bdfd1b
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 07:09:51 +0600

    Update MainLayout.jsx

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,1 +1,19 @@
+function MainLayout({ children }) {
+  return (
+    <div>
+      <header>
+        <h1>IMO Diamond Store BD</h1>
+      </header>
 
+      <main>
+        {children}
+      </main>
+
+      <footer>
+        <p>© IMO Diamond Store BD</p>
+      </footer>
+    </div>
+  );
+}
+
+export default MainLayout;
