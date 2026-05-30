67ca6d36469b2af9a12cce467bb97a24554b2f8f
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:20:26 +0600

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,36 @@
+const User = require("../models/User");
 
+const register = (req, res) => {
+  res.json({
+    success: true,
+    message: "Register endpoint ready"
+  });
+};
+
+const login = (req, res) => {
+  res.json({
+    success: true,
+    message: "Login endpoint ready"
+  });
+};
+
+const googleLogin = (req, res) => {
+  res.json({
+    success: true,
+    message: "Google Login endpoint ready"
+  });
+};
+
+const logout = (req, res) => {
+  res.json({
+    success: true,
+    message: "Logout endpoint ready"
+  });
+};
+
+module.exports = {
+  register,
+  login,
+  googleLogin,
+  logout
+};
