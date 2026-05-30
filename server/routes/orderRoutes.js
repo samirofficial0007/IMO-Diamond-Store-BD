1f034a318287c60c933b8e4e8a96e2d99b32bddf
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:23:41 +0600

    Update orderRoutes.js

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,1 +1,11 @@
+const orderController = require("../controllers/orderController");
 
+const orderRoutes = {
+  createOrder: orderController.createOrder,
+  getUserOrders: orderController.getUserOrders,
+  updateOrderStatus: orderController.updateOrderStatus,
+  approveOrder: orderController.approveOrder,
+  paymentNotFound: orderController.paymentNotFound
+};
+
+module.exports = orderRoutes;
