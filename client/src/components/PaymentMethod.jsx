ee43f847c8a6375f00dc5c15b17ab301a3081dc9
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 08:01:04 +0600

    Update PaymentMethod.jsx

--- client/src/components/PaymentMethod.jsx
+++ client/src/components/PaymentMethod.jsx
@@ -1,1 +1,15 @@
+function PaymentMethod() {
+  return (
+    <div>
+      <h2>Payment Methods</h2>
 
+      <ul>
+        <li>bKash</li>
+        <li>Nagad</li>
+        <li>Binance Pay</li>
+      </ul>
+    </div>
+  );
+}
+
+export default PaymentMethod;
