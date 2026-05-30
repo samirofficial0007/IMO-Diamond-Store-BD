7e721d51850b3d58846596105471cd05593b7eb2
samirofficial0007 <samirboss3323@gmail.com>
2026-05-30 13:39:34 +0600

    Update packageController.js

--- server/controllers/packageController.js
+++ server/controllers/packageController.js
@@ -1,1 +1,36 @@
+const Package = require("../models/Package");
 
+const getPackages = (req, res) => {
+  res.json({
+    success: true,
+    message: "Get Packages endpoint ready"
+  });
+};
+
+const addPackage = (req, res) => {
+  res.json({
+    success: true,
+    message: "Add Package endpoint ready"
+  });
+};
+
+const updatePackage = (req, res) => {
+  res.json({
+    success: true,
+    message: "Update Package endpoint ready"
+  });
+};
+
+const deletePackage = (req, res) => {
+  res.json({
+    success: true,
+    message: "Delete Package endpoint ready"
+  });
+};
+
+module.exports = {
+  getPackages,
+  addPackage,
+  updatePackage,
+  deletePackage
+};
